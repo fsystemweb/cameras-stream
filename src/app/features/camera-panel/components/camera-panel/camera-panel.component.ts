07f53e7d1beee220a36290befc1806f85aa810dc
fed <facu.cappella@gmail.com>
2024-11-16 17:26:52 +0100

    stream camera PoC

--- src/app/features/camera-panel/components/camera-panel/camera-panel.component.ts
+++ src/app/features/camera-panel/components/camera-panel/camera-panel.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { WebcamComponent } from '../webcam/webcam.component';
 import { CameraItem } from '../../../camera-list/models/camera-item';
+import { CameraStreamComponent } from '../camera-stream/camera-stream.component';
 
 @Component({
   selector: 'app-camera-panel',
   standalone: true,
   templateUrl: './camera-panel.component.html',
-  imports: [WebcamComponent],
+  imports: [WebcamComponent, CameraStreamComponent],
 })
 export class CameraPanelComponent {
   camerasActive: CameraItem[] = [];
