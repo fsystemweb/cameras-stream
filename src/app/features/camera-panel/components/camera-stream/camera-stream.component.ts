e174148554aed5176418387cc40b9b4f33370458
Facundo Ezequiel Diaz Cappella <43161950+fsystemweb@users.noreply.github.com>
2024-11-16 17:32:24 +0100

    stream camera PoC (#8)

---
+++ src/app/features/camera-panel/components/camera-stream/camera-stream.component.ts
@@ -0,0 +1,40 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  viewChild,
+} from '@angular/core';
+import Hls from 'hls.js';
+
+@Component({
+  selector: 'app-camera-stream',
+  standalone: true,
+  templateUrl: './camera-stream.component.html',
+  imports: [],
+})
+export class CameraStreamComponent implements AfterViewInit {
+  streamUrl = input.required<string>();
+
+  videoElement =
+    viewChild.required<ElementRef<HTMLVideoElement>>('videoElement');
+
+  ngAfterViewInit(): void {
+    this.startStream();
+  }
+
+  startStream(): void {
+    const video = this.videoElement().nativeElement;
+    const hls = new Hls();
+    const streamUrl = this.streamUrl();
+
+    if (Hls.isSupported()) {
+      hls.loadSource(streamUrl);
+      hls.attachMedia(video);
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = streamUrl;
+    } else {
+      console.error('HLS not supported in this browser');
+    }
+  }
+}
