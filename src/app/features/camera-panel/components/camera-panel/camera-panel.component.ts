f9ce76e955b4e0622e61637fbd0b58c35b428c7f
fed <facu.cappella@gmail.com>
2024-11-16 18:32:05 +0100

    add service

--- src/app/features/camera-panel/components/camera-panel/camera-panel.component.ts
+++ src/app/features/camera-panel/components/camera-panel/camera-panel.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { WebcamComponent } from '../webcam/webcam.component';
 import { CameraItem } from '../../../camera-list/models/camera-item';
 import { CameraStreamComponent } from '../camera-stream/camera-stream.component';
+import { ActiveCamerasService } from '../../../../shared/services/active-cameras.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-camera-panel',
@@ -10,5 +12,17 @@ import { CameraStreamComponent } from '../camera-stream/camera-stream.component'
   imports: [WebcamComponent, CameraStreamComponent],
 })
 export class CameraPanelComponent {
+  private activeCamerasService = inject(ActiveCamerasService);
+  private destroyRef = inject(DestroyRef);
+
   camerasActive: CameraItem[] = [];
+
+  constructor() {
+    this.activeCamerasService
+      .getCameras$()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((cameras) => {
+        this.camerasActive = cameras;
+      });
+  }
 }
---
+++ src/app/shared/services/active-cameras.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CameraItem } from '../../features/camera-list/models/camera-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ActiveCamerasService {
+  private dataSubject: BehaviorSubject<CameraItem[]> = new BehaviorSubject<
+    CameraItem[]
+  >([]);
+
+  addCamera(cameraItem: CameraItem): void {
+    const currentData = this.dataSubject.value;
+    const updatedData = [...currentData, cameraItem];
+    this.dataSubject.next(updatedData);
+  }
+
+  removeCamera(cameraItem: CameraItem): void {
+    const currentData = this.dataSubject.value;
+    const updatedData = currentData.filter((item) => item.id !== cameraItem.id);
+    this.dataSubject.next(updatedData);
+  }
+
+  getCameras$(): Observable<CameraItem[]> {
+    return this.dataSubject.asObservable();
+  }
+}
