c738de771b6c4e524d984653a8daf94977214027
Facundo Ezequiel Diaz Cappella <43161950+fsystemweb@users.noreply.github.com>
2024-11-17 12:10:18 +0100

    Snapshot (#10)
    * add snapshoot feature
    
    * add test

--- src/app/features/camera-panel/components/camera-stream/camera-stream.component.ts
+++ src/app/features/camera-panel/components/camera-stream/camera-stream.component.ts
@@ -19,6 +19,7 @@ export class CameraStreamComponent implements AfterViewInit {
   private streamHelperService = inject(StreamHelperService);
 
   streamUrl = input.required<string>();
+  id = input.required<string>();
 
   videoElement =
     viewChild.required<ElementRef<HTMLVideoElement>>('videoElement');
