6fd37248d4bd8af7c64bf0f74c10de15977cb406
Facundo Ezequiel Diaz Cappella <43161950+fsystemweb@users.noreply.github.com>
2026-02-08 17:30:03 +0100

    feat: new ui/ux (#16)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     './src/**/*.{html,scss,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#3B82F6', // blue-500
+          foreground: '#FFFFFF',
+        },
+        secondary: {
+          DEFAULT: '#60A5FA', // blue-400
+          foreground: '#FFFFFF',
+        },
+        cta: {
+          DEFAULT: '#F97316', // orange-500
+          foreground: '#FFFFFF',
+        },
+        background: '#F8FAFC', // slate-50
+        surface: '#FFFFFF',
+        text: {
+          DEFAULT: '#1E293B', // slate-800
+          muted: '#64748B', // slate-500
+        }
+      },
+      fontFamily: {
+        sans: ['"Fira Sans"', 'sans-serif'],
+        mono: ['"Fira Code"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
